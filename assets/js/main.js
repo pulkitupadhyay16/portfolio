b0a86c977be5c73b278499fa5b898c4c9c5919f8
pulkitupadhyay16 <pulkitu71@gmail.com>
2023-06-11 04:10:02 +0530

    about and skills done

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,6 +32,26 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+        for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+
+        }
+
+        if(itemClass ==='skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+        }
+}
+skillsHeader.forEach((eL)=>{
+    eL.addEventListener('click',toggleSkills)
+})
+ 
+
 
 /*==================== QUALIFICATION TABS ====================*/
 
@@ -54,4 +74,4 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 /*==================== SHOW SCROLL UP ====================*/ 
 
 
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
